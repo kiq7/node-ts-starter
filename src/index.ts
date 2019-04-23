327ee9a028126a0d6185db5741c074444a84dd6f
Kaique Vieira <kaiiquevieira@gmail.com>
2019-04-23 14:52:50 -0300

    refactor: fix linter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from 'express'
 
-const server = express();
+const server = express()
 
 server.get('/', (_, res) => {
-  res.send("Picante 🌶");
-});
+  res.send('Picante 🌶')
+})
 
 server.listen(3000, () => {
-  console.log(`[SERVER] Running at localhost:3000`);
-});
+  console.log(`[SERVER] Running at localhost:3000`)
+})
 
-export { express as server };
+export { express as server }
