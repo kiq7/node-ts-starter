fb6dc2570c8d63a6e254b5e91d07131f037b41bd
Kaique Vieira <kaiiquevieira@gmail.com>
2019-04-30 11:56:13 -0300

    refactor: change file names

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,27 @@
-import { server } from './app'
+import express from 'express'
+import cors from 'cors'
+import { routes } from './routes'
 
-server.listen(3333)
+class Server {
+  public express: express.Application
+
+  public constructor () {
+    this.express = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private routes (): void {
+    this.express.use(routes)
+  }
+}
+
+const server = new Server().express
+
+export { server }
